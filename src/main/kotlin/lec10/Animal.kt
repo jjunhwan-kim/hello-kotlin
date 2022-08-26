a9869a8e70207cc365444d4df29e1e78160f185d
JunHwan Kim <nefertiri972@gmail.com>
2022-08-26 23:13:23 +0900

    10강. 코틀린에서 상속을 다루는 방법

---
+++ src/main/kotlin/lec10/Animal.kt
@@ -0,0 +1,12 @@
+package lec10
+
+abstract class Animal(
+    protected val species: String,
+    protected open val legCount: Int
+) {
+    /**
+     * 추상 프로퍼티가 아니라면 상속받을 때 open을 꼭 붙여야 한다
+     */
+
+    abstract fun move()
+}
---
+++ src/main/kotlin/lec10/Flyable.kt
@@ -0,0 +1,9 @@
+package lec10
+
+interface Flyable {
+
+    // default method
+    fun act() {
+        println("파닥 파닥")
+    }
+}
